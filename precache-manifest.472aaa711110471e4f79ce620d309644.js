37743ee36fbd54a3f733df94ae6260c10146f7e8
Ailun Shen <newhoopsdynasty@gmail.com>
2020-09-22 15:52:38 -0400

    Updates

---
+++ precache-manifest.472aaa711110471e4f79ce620d309644.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93fb2a427d56ac036a97e59bce97909f",
+    "url": "/memegen/index.html"
+  },
+  {
+    "revision": "9bca48aca49d9c13c80f",
+    "url": "/memegen/static/css/main.ccac50d1.chunk.css"
+  },
+  {
+    "revision": "0999d58fc5799e057b9e",
+    "url": "/memegen/static/js/2.e1fb9f6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memegen/static/js/2.e1fb9f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bca48aca49d9c13c80f",
+    "url": "/memegen/static/js/main.16249d8b.chunk.js"
+  },
+  {
+    "revision": "1f13fb36fa0a10d4bfc1",
+    "url": "/memegen/static/js/runtime-main.47c97c6c.js"
+  }
+]);
